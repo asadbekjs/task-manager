2784848b25553c5f11d9731d2becbc0885d313c8
asadbekjs <asadbek_rahimov@mail.ru>
2026-05-20 16:14:40 +0500

    feat: remove task completion statistics from TaskList component for cleaner UI

--- src/components/task-list.jsx
+++ src/components/task-list.jsx
@@ -1,14 +1,8 @@
 import TaskItem from "./task-item";
 
 function TaskList({ tasks, onToggle, onDelete }) {
-  const doneCount = tasks.filter((t) => t.isDone).length;
-  const totalCount = tasks.length;
-
   return (
-    <div style={{ maxWidth: 480, margin: "0 auto" }}>
-      <p style={{ color: "#6b7280", marginBottom: 12 }}>
-        {doneCount}/{totalCount} vazifa bajarildi
-      </p>
+    <div>
       {tasks.map((task) => {
         return (
           <TaskItem
